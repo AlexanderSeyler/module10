16e71d4f16dcdeb41f75b1269f064fb21d20ecd6
AlexanderSeyler <alexanderseyler@gmail.com>
2024-02-20 12:12:46 +0800

    further updates

--- exercise1/index.js
+++ exercise1/index.js
@@ -9,5 +9,5 @@ app.use("/calculator", calculatorRoutes);
 app.use("/", express.static("public"));
 
 app.listen(port, () => {
-  console.log(`New app changes at http://localhost:${port}`);
+  console.log(`updates at http://localhost:${port}`);
 });
